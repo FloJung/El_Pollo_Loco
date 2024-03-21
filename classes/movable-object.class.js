c024c4f50b9bbde5eb548fb94545be5c4ab2f885
FloJung <flo-fj@gmx.at>
2024-03-21 20:11:30 +0100

    add Jumping Pepe

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -8,8 +8,23 @@ class MovableObject{
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
 
+    applyGravity() {
 
+        setInterval(() =>{
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+            
+        },1000/25)
+    }
+
+    isAboveGround() {
+        return this.y < 140;
+    }
 
     loadImage(path) {
         this.img = new Image();
@@ -26,18 +41,20 @@ class MovableObject{
     }
 
     moveRight() {
-
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
     }
-    playAnimation(images){
+    playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WARLKING.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++
+    }
+
+    jump() {
+        this.speedY = 30;
     }
 }
